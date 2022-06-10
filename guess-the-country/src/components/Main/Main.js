1566597313d24c5b29b88d24b2b1fe2d6cca4735
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-06-10 12:32:19 -0400

    commiting so i can pull develop

--- guess-the-country/src/components/Main/Main.js
+++ guess-the-country/src/components/Main/Main.js
@@ -4,13 +4,13 @@ import './Main.scss'
 function Main({ randomCountry }) {
   return (
     <div>
-        <img src={randomCountry.flags.png} alt='Country Flag'/>
+        <img alt='Country Flag'/>
         <div>
           <label>Guess the Country </label>
           <input type='text' name='name' id='name' placeholder='Guess the Country'/>
           <button 
           onClick={() => {
-            randomCountry.clickHandler(target.name.value)
+            randomCountry.clickHandler()
           }}
           >
             Submit</button>
