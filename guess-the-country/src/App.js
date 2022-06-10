596c78ef19d9b4c4af6fb39e162b5c763fd8307f
danielj-84 <danieldimatteo123@gmail.com>
2022-06-10 12:10:27 -0400

    fixed app functionality

--- guess-the-country/src/App.js
+++ guess-the-country/src/App.js
@@ -3,52 +3,58 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import "./App.scss";
 import { Component } from "react";
+import axios from "axios";
+
+const API_URL = "https://restcountries.com/v3.1";
 
 class App extends Component {
   state = {
     countries: [],
     randomCountry: {},
-    count: 0
-}
+    count: 0,
+  };
 
-componentDidMount() {
+  componentDidMount() {
     axios
-        .get(`${API_URL}/all`)
-        .then(response=> {
-            this.setState({
-                countries: response.data,
-            })
-        })
-        .catch(e=> console.log("error in component mounting", e))
-}
+      .get(`${API_URL}/all`)
+      .then((response) => {
+        this.setState({
+          countries: response.data,
+        });
+      })
+      .catch((e) => console.log("error in component mounting", e));
+  }
 
-componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps) {
     // const previousRandomFlag = prevProps.match.params.id;
     // const currentRandomFlag = this.props.match.params.id;
+  }
 
-    if (SUBMIT.toLowerCase() === currentRandomFlag.toLowerCase()) {
-        //MAKE IT SAY CONGRATS
-        //MAKE THE COUNTER GO UP
-        this.getRandomFlag(this.state.countries);
-    }  else {
-        //GO TO GAME OVER PAGE
-    }
-}
-
-getRandomFlag = (array) => {
+  getRandomFlag = (array) => {
     array = this.state.countries;
     const randomCountry = array[Math.floor(Math.random() * Array.length)];
     this.setState({
-        randomCountry: randomCountry,
-    })
-}
+      randomCountry: randomCountry,
+    });
+  };
+
+  clickHandler = () => {
+    if (SUBMIT.toLowerCase() === currentRandomFlag.toLowerCase()) {
+      this.setState({
+        count: count++,
+      });
+      this.getRandomFlag(this.state.countries);
+    } else {
+      //GO TO GAME OVER PAGE
+    }
+  };
 
   render() {
     return (
       <div className="App">
         <Header />
-        <Main randomCountry={this.state.randomCountry}/>
-        <Counter count={this.state.count}/>
+        <Main randomCountry={this.state.randomCountry} clickHandler={this.clickHandler} />
+        <Counter count={this.state.count} />
       </div>
     );
   }
